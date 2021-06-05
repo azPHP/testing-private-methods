0fd985a2402bf08e4cc9fd1619b8c9ec1199a827
Gordon Forsythe <baohx2000@gmail.com>
2021-06-05 09:47:10 -0700

    second test

--- tests/ClassWithPrivateMethodsTest.php
+++ tests/ClassWithPrivateMethodsTest.php
@@ -6,11 +6,53 @@ use PHPUnit\Framework\TestCase;
 
 class ClassWithPrivateMethodsTest extends TestCase
 {
-    public function testViaPublicMethod()
+    public function testViaPublicMethod(): void
     {
         $obj = new ClassWithPrivateMethods();
 
         $result = $obj->aPublicMethod();
         self::assertEquals(0, $result);
     }
+
+    public function testTryPrivateMethod(): void
+    {
+        $this->expectError();
+        $obj = new ClassWithPrivateMethods();
+        $result = $obj->iDoSomethingComplex(true);
+    }
+
+    public function testPrivateMethod(): void
+    {
+        // ?
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// magic!
+//$refl = new \ReflectionClass($this->validateDNS);
+//$method = $refl->getMethod('getAndValidateCurrentRecord');
+//$method->setAccessible(true);
